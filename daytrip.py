2d4aa77566b1587ce4611942904d0e04160aae49
Nathan-Seeley <wntrstk111@gmail.com>
2022-09-17 19:41:13 -0400

    Create values to lists

--- daytrip.py
+++ daytrip.py
@@ -1,5 +1,17 @@
-day_trip_destinations = []
-day_trip_restaurants = []
-day_trip_mode_of_transportation = []
-day_trip_entertainment = []
+day_trip_destinations = ["Seattle", "New York City", "Las Vegas"]
+day_trip_restaurants = ["Ivars", "The Salty Pig", "Primal Steak House"]
+day_trip_mode_of_transportation = ["Plane", "Train", "Automobile"]
+day_trip_entertainment = ["Shop at Pikes Place Market", "See a musical on Broadway",
+"See Blue Man Group"]
 
+
+        
+        
+        
+        
+        
+        
+
+
+
+}
