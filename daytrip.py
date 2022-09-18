a46eb681cc9ab0761d695f08f2c871d97fbeabb9
Nathan-Seeley <wntrstk111@gmail.com>
2022-09-17 20:22:29 -0400

    Add dictionary of final day trip selections

--- daytrip.py
+++ daytrip.py
@@ -1,17 +1,21 @@
 day_trip_destinations = ["Seattle", "New York City", "Las Vegas"]
-day_trip_restaurants = ["Ivars", "The Salty Pig", "Primal Steak House"]
+day_trip_restaurants = ["Ivars", "", " Balthazar", "Primal Steak House"]
 day_trip_mode_of_transportation = ["Plane", "Train", "Automobile"]
-day_trip_entertainment = ["Shop at Pikes Place Market", "See a musical on Broadway",
-"See Blue Man Group"]
+day_trip_entertainment = ["Shop at Pike Place Market", "See a musical on Broadway",
+"Experience Blue Man Group"]
 
-
-        
-        
-        
-        
-        
-        
-
-
-
-}
+day_trip_dictionary = { 
+    "west_coast_Location" : "Seattle", 
+    "east_coast_location" : "New York City", 
+    "no_coast_location" : "Las Vegas",
+    "west_coast_restaurant" : "Ivars",
+    "east_coast_restaurant" : "Balthazar",
+    "no_coast_restaurant" : "Primal Steak House",
+    "air_travel" : "Plane",
+    "track_travel" : "Train",
+    "road_travel" : "Automobile",
+    "shop" : "Pike Place Market",
+    "see_a_show" : "See a musical on Broadway",
+    "see_an_act" : "Experience Blue Man Group"
+    }
+    
