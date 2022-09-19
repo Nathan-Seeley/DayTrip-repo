d04a02e65cc6186a882e2c3730d127bfbdd4b60f
Nathan-Seeley <wntrstk111@gmail.com>
2022-09-19 15:01:09 -0400

    Create users choice of restaurant within available list

--- daytrip.py
+++ daytrip.py
@@ -18,15 +18,35 @@ def user_destinations_choice ():
 
 user_destination = user_destinations_choice ()
 
+day_trip_restaurants = ["Ivars", "", " Balthazar", "Primal Steak House"]
+
+def user_restaurants_choice ():
+    random_restaurants = random.choice (day_trip_restaurants)
+    user_restaurants_input = input (f"Do you want to dine at {random_restaurants}? (y/n) ")
+    while user_restaurants_input != "y":
+        if user_restaurants_input == "n":
+            random_restaurants = random.choice (day_trip_restaurants)
+            random_restaurants_input = input (f"Do you want to dine at {random_restaurants} instead? (y/n) ")
+        else:
+            print ("Albiet your answer is an answer. Please use y or n to complete.")
+            user_restaurants_input = input (f"Do you want to dine at {random_restaurants}? (y/n) ")
+    return random_restaurants
+
+user_restaurant = user_restaurants_choice ()
 
 
-# day_trip_restaurants = ["Ivars", "", " Balthazar", "Primal Steak House"]
+
+
+
+
+
+
+
 # day_trip_mode_of_transportation = ["Plane", "Train", "Automobile"]
 # day_trip_entertainment = ["Shop at Pike Place Market", "See a musical on Broadway",
 # "Experience Blue Man Group"]
 
 
-# random_restaurants = random.choice (day_trip_restaurants)
 
 # random_transportation = random.choice (day_trip_mode_of_transportation)
 
